a08e22b583aa1297b967210f93ec4225761f663c
Bassem <41355488+account-me@users.noreply.github.com>
2023-06-11 21:27:19 +0300

    Create script.js

---
+++ 1/script.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+  var imageList = document.getElementById("image-list");
+  var images = [];
+
+  // قم بجلب جميع الصور في المجلد
+  fetchImages();
+
+  function fetchImages() {
+    // احصل على قائمة الملفات في المجلد الحالي
+    fetch('./')
+      .then(response => response.text())
+      .then(data => {
+        var parser = new DOMParser();
+        var html = parser.parseFromString(data, 'text/html');
+        var files = Array.from(html.getElementsByTagName('a')).map(a => a.getAttribute('href'));
+        
+        // احصل على الصور فقط واحفظها في المصفوفة "images"
+        images = files.filter(file => /\.(jpe?g|png|gif)$/i.test(file));
+        
+        // عرض أسماء الصور
+        displayImages();
+      });
+  }
+
+  function displayImages() {
+    images.forEach(image => {
+      var listItem = document.createElement("li");
+      listItem.textContent = image;
+      imageList.appendChild(listItem);
+    });
+  }
+};
